baa12a41cb15bb676d9edb3191132200d2bfc62b
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-02 19:26:52 -0500

    Update script.js

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,60 +1,68 @@
 // Assignment code here
-var specialCharacters = [
+ 
+import java.util.Scanner;
 
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  '.',
-  ')',
-  '(',
-  '{',
-  '}',
-  '[',
-  ']',
-  '~',
-  '-',
-  '_',
-  ];
+class Main {
+  public static void main (String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("How many random passwords do you want generated?");
+    int total = in.nextInt();
+    System.out.print("How many characters long do you want your password to be?");
+    int length = in.nextInt();
+    
+    String[] randomPasswords = new String[total];
+    
+    for(int i - 0; i < length; i++) {
+      String randomPassword = "";
+      for(int j = 0; j < length; j++){
+    randomPassword += randomCharacter();
+  }
+randomsPasswords[i] = randomPassword;
+    }
+    
+    printPasswords(randomPasswords);
+    
+    System.out.println(getPasswordStrength(length));
+    
+  }
 
-var numericalCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+public static String getPasswordStrength(int length) {
+if(length < 5) {
+  return "weak";
+}else if(length < 10){
+}else{
+  return "strong";
+}
+}
 
-var lowerCasedCharacters = [
+
+public static void printPasswords(String[] arr) {
+  for(int i = 0; i < arr.length; i++) {
+    System.out.println(arr[i]);
+  }
+}
+public static char randomCharacter() {
   
-'a',
-'b',
-'c',
-'d',
-'e',
-'f',
-'g',
-'h',
-'i',
-'j',
-'k',
-'l',
-'m',
-'n',
-'o',
-'p',
-'q',
-'r',
-'s',
-'t',
-'u',
-'v',
-'w',
-'x',
-'y',
-'z',  
+  int rand = (int) (Math.random()*62);
+
+  if (rand <= 9) {
+   int ascii = rand += 48;
+    return (char) (ascii);
+    
+  } else if (rand <= 35) {
+    
+    int ascii = rand + ;
+    return (char) (ascii);
+  } else {
+    
+    int ascii = rand + 61;
+    return (char) (ascii);
+  }
+  
+}
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
