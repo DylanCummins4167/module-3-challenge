7fc4af3fdf2adca46f947a9de7dd5394a4c14de1
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-05 23:43:38 -0500

    Update script.js

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,79 +1,145 @@
-// Assignment code here
- 
-import java.util.Scanner;
-
-class Main {
-  public static void main (String[] args) {
-    Scanner in = new Scanner(System.in);
-    
-    System.out.print("How many random passwords do you want generated?");
-    int total = in.nextInt();
-    System.out.print("How many characters long do you want your password to be?");
-    int length = in.nextInt();
-    
-    String[] randomPasswords = new String[total];
-    
-    for(int i - 0; i < length; i++) {
-      String randomPassword = "";
-      for(int j = 0; j < length; j++){
-    randomPassword += randomCharacter();
-  }
-randomsPasswords[i] = randomPassword;
-    }
-    
-    printPasswords(randomPasswords);
-    
-    System.out.println(getPasswordStrength(length));
-    
-  }
-
-public static String getPasswordStrength(int length) {
-if(length < 5) {
-  return "weak";
-}else if(length < 10){
-}else{
-  return "strong";
-}
-}
-
-
-public static void printPasswords(String[] arr) {
-  for(int i = 0; i < arr.length; i++) {
-    System.out.println(arr[i]);
-  }
-}
-public static char randomCharacter() {
-  
-  int rand = (int) (Math.random()*62);
-
-  if (rand <= 9) {
-   int ascii = rand += 48;
-    return (char) (ascii);
-    
-  } else if (rand <= 35) {
-    
-    int ascii = rand + ;
-    return (char) (ascii);
-  } else {
-    
-    int ascii = rand + 61;
-    return (char) (ascii);
-  }
-  
-}
-
-}
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var specialSymbols =
+  [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.',
+  ]
+
+var lowerCase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+var upperCase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+function randomCharacter(characterList) {
+  var randomIndex = Math.floor(Math.random() * characterList.length);
+
+  return characterList[randomIndex];
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var randomPassword = '';
+
+  var length = prompt("How many characters long do you want your password to be?");
+
+  // Check if they typed in a valid number
+  if (isNaN(length)) {
+    alert("Enter a valid number.")
+    return;
+  }
+
+  // Convert to a number
+  length = Number(length);
+
+  // Ask which types the user wants for their password
+  var wantNumbers = confirm("Click OK to add numbers to your password.");
+  var wantLowerCases = confirm("Click OK to add lower case letters to your password.");
+  var wantUpperCases = confirm("Click OK to add upper case letters to your password.");
+  var wantSymbols = confirm("Click OK to add special characters to your password.");
+
+  // Array to store wanted characters
+  var charactersList = [];
+
+  // Add the wanted types to the charactersList
+  if (wantNumbers === true) {
+    charactersList = charactersList.concat(numbers);
+  }
+
+  if (wantLowerCases === true) {
+    charactersList = charactersList.concat(lowerCase);
+  }
+  if (wantUpperCases=== true) {
+    charactersList = charactersList.concat(upperCase);
+  }
+  if (wantSymbols === true) {
+    charactersList = charactersList.concat(specialSymbols);
+  }
+
+  for(var i = 0; i < length; i++) {
+    randomPassword += randomCharacter(charactersList)
+  }
+
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = randomPassword;
 }
 
 // Add event listener to generate button
